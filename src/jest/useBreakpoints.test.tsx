343196371834179f2d2e3931889011ffe12e0984
carpenjk <56424605+carpenjk@users.noreply.github.com>
2023-01-17 12:27:44 -0500

    test(usebreakpoints.test.tsx): added tests

--- src/jest/useBreakpoints.test.tsx
+++ src/jest/useBreakpoints.test.tsx
@@ -1,6 +1,6 @@
 import { BreakpointProps } from '@carpenjk/prop-x-types';
-import { beforeEach, afterEach, jest, test, expect } from '@jest/globals';
-import { render, act, renderHook, RenderHookResult } from '@testing-library/react'
+import { test, expect, describe } from '@jest/globals';
+import { renderHook } from '@testing-library/react'
 import useBreakpoint from '../useBreakpoints/useBreakpoints'
 const props = {
   theme: {
@@ -15,62 +15,162 @@ const props = {
 };
 
 const { theme: { breakpoints } }: BreakpointProps = props;
-
-let windowSpy: any;
-
-beforeEach(() => {
-  windowSpy = jest.spyOn(window, "window", "get");
-});
-
-afterEach(() => {
-  windowSpy.mockRestore();
-});
-
-
-// function setup(props: BreakpointProps) {
-//   const returnVal = {};
-//   function TestComponent(): null {
-//     Object.assign(returnVal, useBreakpoint(props));
-//     return null;
-//   }
-//   // render(<TestComponent />);
-
-//   // return returnVal;
-//   return result;
-// }
-
+const SM_WIDTH = 399;
+const MD_WIDTH = 799;
+const LG_WIDTH = 1000;
+const HEIGHT = 1080
 
 // Function: getBackgroundColor
-test('useBreakpoints', async () => {
-  let hookResult: any = {}
-  await act(async () => {
-    hookResult = renderHook(() => useBreakpoint(breakpoints));
-  })
-  console.log("🚀 ~ file: useBreakpoints.test.tsX:48 ~ awaitact ~ hookResult", hookResult)
-  windowSpy.mockImplementation(() => ({
-    innerWidth: 799,
-    innerHeight: 1080,
-  }));
+describe('useBreakpoints', () => {
+  test(`SM_WIDTH: ${SM_WIDTH} `, () => {
+    Object.defineProperty(window, 'innerWidth', {
+      writable: true,
+      configurable: true,
+      value: SM_WIDTH,
+    });
+    Object.defineProperty(window, 'innerHeight', {
+      writable: true,
+      configurable: true,
+      value: HEIGHT,
+    });
 
-  // const br = setup(breakpoints)
-  const br = hookResult?.result?.current || {}
-  const upper = 1200;
-  const lower = 800;
-  expect(br).toEqual({
-    br: {
-      0: 0,
-      1: 400,
-      2: 800,
-      3: 1200
-    },
-    breakpoints: breakpoints,
-    upper: upper,
-    lower: lower,
-    indexOfLower: "2",
-    ratio: 1 - (upper - lower) / upper, // in consideration for future intellegent br functionality
-    current: { width: 1024, height: 768 },
-    toArray: expect.any(Function)
+    let hookResult: any = {}
+    hookResult = renderHook(() => useBreakpoint(breakpoints));
+
+    const br = hookResult?.result?.current || {}
+    const upper = 400;
+    const lower = 0;
+    expect(br).toEqual({
+      br: {
+        0: 0,
+        1: 400,
+        2: 800,
+        3: 1200
+      },
+
+      breakpoints: breakpoints,
+      upper: upper,
+      lower: lower,
+      indexOfLower: "0",
+      ratio: 1 - (upper - lower) / upper, // in consideration for future intellegent br functionality
+      current: { width: SM_WIDTH, height: HEIGHT },
+      toArray: expect.any(Function)
+    });
+    expect(br.toArray())
+      .toEqual([0, 400, 800, 1200])
   });
-  // expect(br.toArray())
-  //   .toEqual([0, 400, 800, 1200])
+
+  test(`MD_WIDTH: ${MD_WIDTH} `, () => {
+    Object.defineProperty(window, 'innerWidth', {
+      writable: true,
+      configurable: true,
+      value: MD_WIDTH,
+    });
+    Object.defineProperty(window, 'innerHeight', {
+      writable: true,
+      configurable: true,
+      value: HEIGHT,
+    });
+
+    let hookResult: any = {}
+    hookResult = renderHook(() => useBreakpoint(breakpoints));
+
+    const br = hookResult?.result?.current || {}
+    const upper = 800;
+    const lower = 400;
+    expect(br).toEqual({
+      br: {
+        0: 0,
+        1: 400,
+        2: 800,
+        3: 1200
+      },
+
+      breakpoints: breakpoints,
+      upper: upper,
+      lower: lower,
+      indexOfLower: "1",
+      ratio: 1 - (upper - lower) / upper, // in consideration for future intellegent br functionality
+      current: { width: MD_WIDTH, height: HEIGHT },
+      toArray: expect.any(Function)
+    });
+    expect(br.toArray())
+      .toEqual([0, 400, 800, 1200])
+  });
+
+  test(`LG_WIDTH: ${LG_WIDTH} `, () => {
+    Object.defineProperty(window, 'innerWidth', {
+      writable: true,
+      configurable: true,
+      value: LG_WIDTH,
+    });
+    Object.defineProperty(window, 'innerHeight', {
+      writable: true,
+      configurable: true,
+      value: HEIGHT,
+    });
+
+    let hookResult: any = {}
+    hookResult = renderHook(() => useBreakpoint(breakpoints));
+
+    const br = hookResult?.result?.current || {}
+    const upper = 1200;
+    const lower = 800;
+    expect(br).toEqual({
+      br: {
+        0: 0,
+        1: 400,
+        2: 800,
+        3: 1200
+      },
+
+      breakpoints: breakpoints,
+      upper: upper,
+      lower: lower,
+      indexOfLower: "2",
+      ratio: 1 - (upper - lower) / upper, // in consideration for future intellegent br functionality
+      current: { width: LG_WIDTH, height: HEIGHT },
+      toArray: expect.any(Function)
+    });
+    expect(br.toArray())
+      .toEqual([0, 400, 800, 1200])
+  });
+
+  test(`EDGE WIDTH: 800 `, () => {
+    Object.defineProperty(window, 'innerWidth', {
+      writable: true,
+      configurable: true,
+      value: 800,
+    });
+    Object.defineProperty(window, 'innerHeight', {
+      writable: true,
+      configurable: true,
+      value: HEIGHT,
+    });
+
+    let hookResult: any = {}
+    hookResult = renderHook(() => useBreakpoint(breakpoints));
+
+    const br = hookResult?.result?.current || {}
+    const upper = 1200;
+    const lower = 800;
+    expect(br).toEqual({
+      br: {
+        0: 0,
+        1: 400,
+        2: 800,
+        3: 1200
+      },
+
+      breakpoints: breakpoints,
+      upper: upper,
+      lower: lower,
+      indexOfLower: "2",
+      ratio: 1 - (upper - lower) / upper, // in consideration for future intellegent br functionality
+      current: { width: 800, height: HEIGHT },
+      toArray: expect.any(Function)
+    });
+    expect(br.toArray())
+      .toEqual([0, 400, 800, 1200])
+  });
 });
